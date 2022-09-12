16b6ad911d54ac26a40c69e6b86d02bf6b0054f0
Olivier Pichon de Vendeuil <108340085+Vendeuil@users.noreply.github.com>
2022-09-12 14:20:42 +0200

    Delete Create_User.sql
    Rodrigue et Karim disent que c'est Spring qui générera les ordres SQL de DDL

--- Create_User.sql
+++
@@ -1,9 +0,0 @@
--- Dialecte PostgreSQL
--- Scribe : Vendeuil
-CREATE TABLE user (
-  iduser SERIAL PRIMARY KEY
-, firstname VARCHAR
-, lastname VARCHAR
-, email VARCHAR
-);
-ALTER TABLE user ADD CHECK (firstname <> lastname);
